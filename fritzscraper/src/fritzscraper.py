e01de10f010a68a41bb20ef12503ecc0f0425a39
jodli <job87@web.de>
2016-11-19 18:23:33 +0100

    Added more data from FritzBox.

--- fritzscraper/src/fritzscraper.py
+++ fritzscraper/src/fritzscraper.py
@@ -3,6 +3,8 @@
 import paho.mqtt.client as mqtt
 import fritzconnection as fc
 
+import time
+
 FRITZ_IP_ADDRESS = '192.168.0.1'
 FRITZ_TCP_PORT = 49000
 
@@ -19,6 +21,9 @@ class FritzScraper(object):
     if connection is None:
       connection = fc.FritzConnection(address=address, port=port)
     self.connection = connection
+    self.last_bytes_sent = self.bytes_sent 
+    self.last_bytes_received = self.bytes_received
+    self.last_traffic_call = time.time()
 
   @property
   def modelname(self):
@@ -51,8 +56,41 @@ class FritzScraper(object):
     """uptime in seconds."""
     return self.connection.call_action('WANIPConnection', 'GetStatusInfo')['NewUptime']
 
-  def connect_fritz():
-    print("Connecting to FritzBox...")
+  @property
+  def bytes_received(self):
+    return self.connection.call_action('WANCommonInterfaceConfig', 'GetTotalBytesReceived')['NewTotalBytesReceived']
+
+  @property
+  def bytes_sent(self):
+    return self.connection.call_action('WANCommonInterfaceConfig', 'GetTotalBytesSent')['NewTotalBytesSent']
+
+  @property
+  def transmission_rate(self):
+    """Returns the upstream, downstream values as a tuple in bytes per second. Use this for periodical calling."""
+    sent = self.bytes_sent
+    received = self.bytes_received
+    traffic_call = time.time()
+    time_delta = traffic_call - self.last_traffic_call
+    upstream = int(1.0 * (sent - self.last_bytes_sent)/time_delta)
+    downstream = int(1.0 * (received - self.last_bytes_received)/time_delta)
+    self.last_bytes_sent = sent
+    self.last_bytes_received = received
+    self.last_traffic_call = traffic_call
+    return upstream, downstream
+
+  @property
+  def max_bit_rate(self):
+    """Returns a tuple with the maximun upstream- and downstream-rate of the given connection. The rate is given in bits/sec."""
+    status = self.connection.call_action('WANCommonInterfaceConfig', 'GetCommonLinkProperties')
+    downstream = status['NewLayer1DownstreamMaxBitRate']
+    upstream = status['NewLayer1UpstreamMaxBitRate']
+    return upstream, downstream
+
+  @property
+  def max_byte_rate(self):
+    """Same as max_bit_rate but returns the rate in bytes/sec."""
+    upstream, downstream = self.max_bit_rate
+    return upstream / 8.0, downstream / 8.0
 
 if __name__ == '__main__':
   connect_mqtt()
@@ -63,4 +101,9 @@ if __name__ == '__main__':
   print('wan access: ' + scraper.wan_access_type)
   print('external ip: ' + scraper.external_ip)
   print('uptime: ' + str(scraper.uptime))
+  print('bytes sent: ' + str(scraper.bytes_sent))
+  print('bytes received: ' + str(scraper.bytes_received))
+  print('bytes received / s: ' + str(scraper.transmission_rate))
+  print('max bit rate: ' + str(scraper.max_bit_rate))
+  print('max byte rate: ' + str(scraper.max_byte_rate))
   scrape()
