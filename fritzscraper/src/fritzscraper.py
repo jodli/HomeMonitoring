1c7849d1191bc933a9ee0a6cd41a0aaf8f91b366
jodli <job87@web.de>
2016-11-20 13:24:45 +0100

    Moved around some code and renamed classes

--- fritzscraper/src/fritzscraper.py
+++ fritzscraper/src/fritzscraper.py
@@ -1,91 +1,44 @@
-#!/usr/bin/env python
 
-import fritzconnection as fc
-
+import scraper as fs
+import mqttconnection as mqtt
+import signal
+import sys
 import time
-
-FRITZ_IP_ADDRESS = '192.168.0.1'
-FRITZ_TCP_PORT = 49000
 
 class FritzScraper(object):
 
-  def __init__(self, address=FRITZ_IP_ADDRESS, port=FRITZ_TCP_PORT):
-    connection = fc.FritzConnection(address=address, port=port)
-    print("Connected to FritzBox")
+  def __init__(self):
+    self._exit = False
+    signal.signal(signal.SIGTERM, self._sigterm_handler)
 
-    self.connection = connection
-    self.last_bytes_sent = self.bytes_sent 
-    self.last_bytes_received = self.bytes_received
-    self.last_traffic_call = time.time()
+    self._scraper = fs.Scraper()
+    #self._mqttConnection = mqtt.MqttConnection()
 
-  @property
-  def modelname(self):
-    return self.connection.modelname
+    #self._mqttConnection.connect()
 
-  @property
-  def is_linked(self):
-    status = self.connection.call_action('WANCommonInterfaceConfig', 'GetCommonLinkProperties')
-    return status['NewPhysicalLinkStatus'] == 'Up'
+  def _sigterm_handler(self, signal, frame):
+    print("SIGTERM received")
+    self._exit = True
 
-  @property
-  def is_connected(self):
-    status = self.connection.call_action('WANIPConnection', 'GetStatusInfo')
-    return status['NewConnectionStatus'] == 'Connected'
+  def run(self):
+    while not self._exit:
+      print("Working...")
+      sys.stdout.flush()
+      time.sleep(1)
+      #self._scraper.print_status()
 
-  @property
-  def wan_access_type(self):
-    return self.connection.call_action('WANCommonInterfaceConfig', 'GetCommonLinkProperties')['NewWANAccessType']
+  def close(self):
+    print("Cleaning up...")
 
-  @property
-  def external_ip(self):
-    return self.connection.call_action('WANIPConnection', 'GetExternalIPAddress')['NewExternalIPAddress']
+if __name__ == '__main__':
 
-  @property
-  def uptime(self):
-    return self.connection.call_action('WANIPConnection', 'GetStatusInfo')['NewUptime']
+  try:
+    print("Creating FritzScraper...")
+    fritzScraper = FritzScraper()
+  except Exception as e:
+    sys.exit("Could not start FritzScraper: " + str(e))
+  else:
+    print("Start scraping...")
+    fritzScraper.run()
 
-  @property
-  def bytes_received(self):
-    return self.connection.call_action('WANCommonInterfaceConfig', 'GetTotalBytesReceived')['NewTotalBytesReceived']
-
-  @property
-  def bytes_sent(self):
-    return self.connection.call_action('WANCommonInterfaceConfig', 'GetTotalBytesSent')['NewTotalBytesSent']
-
-  @property
-  def transmission_rate(self):
-    sent = self.bytes_sent
-    received = self.bytes_received
-    traffic_call = time.time()
-    time_delta = traffic_call - self.last_traffic_call
-    upstream = int(1.0 * (sent - self.last_bytes_sent)/time_delta)
-    downstream = int(1.0 * (received - self.last_bytes_received)/time_delta)
-    self.last_bytes_sent = sent
-    self.last_bytes_received = received
-    self.last_traffic_call = traffic_call
-    return upstream, downstream
-
-  @property
-  def max_bit_rate(self):
-    status = self.connection.call_action('WANCommonInterfaceConfig', 'GetCommonLinkProperties')
-    downstream = status['NewLayer1DownstreamMaxBitRate']
-    upstream = status['NewLayer1UpstreamMaxBitRate']
-    return upstream, downstream
-
-  @property
-  def max_byte_rate(self):
-    upstream, downstream = self.max_bit_rate
-    return upstream / 8.0, downstream / 8.0
-
-  def print_status(self):
-    print('model: ' + self.modelname)
-    print('linked: ' + str(self.is_linked))
-    print('connected: ' + str(self.is_connected))
-    print('wan access: ' + self.wan_access_type)
-    print('external ip: ' + self.external_ip)
-    print('uptime: ' + str(self.uptime))
-    print('bytes sent: ' + str(self.bytes_sent))
-    print('bytes received: ' + str(self.bytes_received))
-    print('bytes received / s: ' + str(self.transmission_rate))
-    print('max bit rate: ' + str(self.max_bit_rate))
-    print('max byte rate: ' + str(self.max_byte_rate))
+    fritzScraper.close()
--- fritzscraper/src/scraper.py
+++ fritzscraper/src/scraper.py
@@ -1,15 +1,91 @@
+#!/usr/bin/env python
 
-import fritzscraper as fs
-import mqttconnection as mqtt
+import fritzconnection as fc
 
-if __name__ == '__main__':
-  scraper = fs.FritzScraper()
-  scraper.print_status()
+import time
 
-  mqttConn = mqtt.MqttConnection()
-  try:
-    mqttConn.connect()
-  except KeyboardInterrupt:
-    print('Interrupt received, cleaning up...')
-  finally:
-    mqttConn.disconnect()
+FRITZ_IP_ADDRESS = '192.168.0.1'
+FRITZ_TCP_PORT = 49000
+
+class Scraper(object):
+
+  def __init__(self, address=FRITZ_IP_ADDRESS, port=FRITZ_TCP_PORT):
+    connection = fc.FritzConnection(address=address, port=port)
+    print("Connected to FritzBox")
+
+    self.connection = connection
+    self.last_bytes_sent = self.bytes_sent 
+    self.last_bytes_received = self.bytes_received
+    self.last_traffic_call = time.time()
+
+  @property
+  def modelname(self):
+    return self.connection.modelname
+
+  @property
+  def is_linked(self):
+    status = self.connection.call_action('WANCommonInterfaceConfig', 'GetCommonLinkProperties')
+    return status['NewPhysicalLinkStatus'] == 'Up'
+
+  @property
+  def is_connected(self):
+    status = self.connection.call_action('WANIPConnection', 'GetStatusInfo')
+    return status['NewConnectionStatus'] == 'Connected'
+
+  @property
+  def wan_access_type(self):
+    return self.connection.call_action('WANCommonInterfaceConfig', 'GetCommonLinkProperties')['NewWANAccessType']
+
+  @property
+  def external_ip(self):
+    return self.connection.call_action('WANIPConnection', 'GetExternalIPAddress')['NewExternalIPAddress']
+
+  @property
+  def uptime(self):
+    return self.connection.call_action('WANIPConnection', 'GetStatusInfo')['NewUptime']
+
+  @property
+  def bytes_received(self):
+    return self.connection.call_action('WANCommonInterfaceConfig', 'GetTotalBytesReceived')['NewTotalBytesReceived']
+
+  @property
+  def bytes_sent(self):
+    return self.connection.call_action('WANCommonInterfaceConfig', 'GetTotalBytesSent')['NewTotalBytesSent']
+
+  @property
+  def transmission_rate(self):
+    sent = self.bytes_sent
+    received = self.bytes_received
+    traffic_call = time.time()
+    time_delta = traffic_call - self.last_traffic_call
+    upstream = int(1.0 * (sent - self.last_bytes_sent)/time_delta)
+    downstream = int(1.0 * (received - self.last_bytes_received)/time_delta)
+    self.last_bytes_sent = sent
+    self.last_bytes_received = received
+    self.last_traffic_call = traffic_call
+    return upstream, downstream
+
+  @property
+  def max_bit_rate(self):
+    status = self.connection.call_action('WANCommonInterfaceConfig', 'GetCommonLinkProperties')
+    downstream = status['NewLayer1DownstreamMaxBitRate']
+    upstream = status['NewLayer1UpstreamMaxBitRate']
+    return upstream, downstream
+
+  @property
+  def max_byte_rate(self):
+    upstream, downstream = self.max_bit_rate
+    return upstream / 8.0, downstream / 8.0
+
+  def print_status(self):
+    print('model: ' + self.modelname)
+    print('linked: ' + str(self.is_linked))
+    print('connected: ' + str(self.is_connected))
+    print('wan access: ' + self.wan_access_type)
+    print('external ip: ' + self.external_ip)
+    print('uptime: ' + str(self.uptime))
+    print('bytes sent: ' + str(self.bytes_sent))
+    print('bytes received: ' + str(self.bytes_received))
+    print('bytes received / s: ' + str(self.transmission_rate))
+    print('max bit rate: ' + str(self.max_bit_rate))
+    print('max byte rate: ' + str(self.max_byte_rate))
