6b804b56390d265ff7b9fec8aa9bb5a55aa3e247
jodli <job87@web.de>
2016-11-20 13:22:19 +0100

    Added support for signals in python bg process

--- fritzscraper/src/startup.sh
+++ fritzscraper/src/startup.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
+set -x
 
-pip install -r requirements.txt --upgrade
+pid=0
 
-python scraper.py
+sigterm_handler() {
+  if [ $pid -ne 0 ]; then
+    kill -SIGTERM "$pid"
+    wait "$pid"
+  fi
+  exit 143;
+}
+
+trap 'kill ${!}; sigterm_handler' SIGTERM
+
+#pip install -r requirements.txt --upgrade
+
+python fritzscraper.py &
+pid="$!"
+
+while true
+do
+  tail -f /dev/null & wait ${!}
+done
